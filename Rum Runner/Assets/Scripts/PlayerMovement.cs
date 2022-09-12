f636cc90bf6091e8cebddd5832b98fcb0a7fb7ae
JordanR <jar5927@rit.edu>
2022-09-11 20:57:32 -0400

    Rigidbody movement

--- Rum Runner/Assets/Scripts/PlayerMovement.cs
+++ Rum Runner/Assets/Scripts/PlayerMovement.cs
@@ -1,126 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    #region  Player movement
-    public Vector3 playerPosition = Vector3.zero;
-    public Vector3 direction = Vector3.right;
-    public Vector3 velocity = Vector3.zero;
-    #endregion
 
-    public float speed = 5f;
-    public float maxSpeed = 5f;
+    [Header("Movement Variables")]
+    private Rigidbody2D rb;
+    private Vector2 moveVector;
+    public float jumpDistance;
+    public float moveSpeed;
+    [SerializeField] bool isFacingRight;
 
-    public float jumpForce= 5f;
-    public Vector3 gravity = Vector3.down;
+    [Header("Ground Check")]
+    [SerializeField] bool isGrounded;
+    public Transform groundSpot;
+    public LayerMask groundLayer;
 
-    public Vector2 playerHorizInput;
-    public bool playerJumping;
+    private float normalGravity;
 
-    #region Camera vars
-    private Camera cam;
-    private float halfCamHeight;
-    private float halfCamWidth;
-    #endregion
-
-    void Start()
+    private void Awake()
     {
-        //  Set the starting Spaceship position to the current position of the GameObject
-        playerPosition = transform.position;
-
-        gravity *= 3f;
-        jumpForce = 5f;
-        
-
-        // Set camera variables
-        cam = Camera.main;
-        halfCamHeight = cam.orthographicSize;
-        halfCamWidth = halfCamHeight * cam.aspect;
+        rb = GetComponent<Rigidbody2D>();
+        normalGravity = rb.gravityScale;
+        isFacingRight = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Move(InputAction.CallbackContext context)
     {
-        playerJumping = false;
-
-        //velocity += gravity * Time.deltaTime;
-        if (playerHorizInput.x > 0)
-        {
-            // Move Right
-            direction = Vector3.right;
-            velocity += speed * direction * Time.deltaTime;
-        }else if (playerHorizInput.x < 0){
-            // Move Left
-            direction = Vector3.left;
-            velocity += speed * direction * Time.deltaTime;
-        }
-       // else if (playerJumping){
-            // Move up
-       //     direction = Vector3.up;
-       //     velocity += jumpForce * direction * Time.deltaTime;
-       // }
-        else
-        {
-            velocity = Vector3.zero;
-        }
-
-         // Limit Velocity to maxSpeed
-        velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
-
-        // Calc new position
-        playerPosition += velocity * Time.deltaTime;
-
-        //  Check to make sure the Player is still on the screen
-        BounceOffWalls();
-
-        // Appy the movement to the GameObject
-        transform.position = playerPosition;
+        moveVector = context.ReadValue<Vector2>();
     }
 
-    // Check if the Player has left the screen
-    // if so move it back same side
-    void BounceOffWalls()
+    public void Jump(InputAction.CallbackContext context)
     {
-        // Spaceship Position Wrapping
-        // Goes off the Right
-        if (playerPosition.x > halfCamWidth)
+        if(context.performed && isGrounded)
         {
-            velocity.x *= -1f;
-        }
-
-        // Goes off the Left
-        if (playerPosition.x < -halfCamWidth)
-        {
-            velocity.x *= -1f;
-        }
-
-        // Goes off the Top
-        if (playerPosition.y > halfCamHeight)
-        {
-            playerPosition.y = halfCamHeight;
-            transform.position = playerPosition;
-            //velocity.y *= -1f;
-        }
-
-        // Goes off the Bottom
-        if (playerPosition.y < -halfCamHeight)
-        {
-            playerPosition.y = -halfCamHeight;
-            transform.position = playerPosition;
-            //velocity.y *= -1f;
+            rb.velocity = new Vector2(rb.velocity.x, jumpDistance);
         }
     }
 
-    public void OnMove(InputValue Value)
+    private void Update()
     {
-        playerHorizInput = Value.Get<Vector2>();
-        Debug.Log("move:" + playerHorizInput);
+        isGrounded = Physics2D.OverlapCircle(groundSpot.position, 0.1f, groundLayer);
     }
 
-    public void OnJump(InputValue Value)
+    private void FixedUpdate()
     {
-        playerJumping = Value.isPressed;
-        Debug.Log("move:" + Value.isPressed);
+        rb.velocity = new Vector2(moveVector.x * moveSpeed, rb.velocity.y);
     }
+
 }
