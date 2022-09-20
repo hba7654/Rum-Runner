b267006cc818bc9b4f4987ffe1ec7529cd1d596b
Cameron Adams <cka8762@rit.edu>
2022-09-20 16:22:54 -0400

    Started working on shooting
    bullets are created on click, they fly using the rigidbody physics system and destroy themselves after a certain point. MousePosition aiming has been started but not finished.

---
+++ Rum Runner/Assets/Scripts/PlayerUtility.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerUtility : MonoBehaviour
+{
+    [Header("Imported Objects")]
+    public GameObject bullet;
+    private Bullet bulletScript;
+    public Camera mainCam;
+
+    [Header("General Variables")]
+    private Vector2 mousePosition;
+
+
+    [Header("Shooting Variables")]
+    private float bulletSpeed;
+
+    public void Awake()
+    {
+        bulletSpeed = 5f;
+       
+    }
+
+    public void Fire()
+    {
+        GameObject bulletClone;
+        Vector2 bulletSpawnPosition = new Vector2(transform.position.x + 0.5f, transform.position.y);
+        bulletClone = Instantiate(bullet, bulletSpawnPosition, transform.rotation);
+        bulletScript = bulletClone.GetComponent<Bullet>();
+        //mousePosition = GetMousePosition();
+        bulletScript.InitialMove(bulletSpeed, new Vector2(1f,0f));
+    }
+
+    public void GetMousePosition(InputAction.CallbackContext context)
+    {
+        mousePosition = context.ReadValue<Vector2>();
+    }
+
+
+}
