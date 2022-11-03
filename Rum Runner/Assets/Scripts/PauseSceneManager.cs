d2cceed3f8846fd40c76a2724dde02b6634476ac
JordanR <jar5927@rit.edu>
2022-11-03 15:34:11 -0400

    Trying to fix pause bug

--- Rum Runner/Assets/Scripts/PauseSceneManager.cs
+++ Rum Runner/Assets/Scripts/PauseSceneManager.cs
@@ -1,16 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class PauseSceneManager : MonoBehaviour
 {
+    [SerializeField] Button resumeBttn;
+    [SerializeField] Button resetBttn;
+    [SerializeField] Button quitBttn;
+
+    private void Update()
+    {
+        resumeBttn.onClick.AddListener(Resume);
+/*        if (resumeBttn.onClick)
+        {
+            GameManager.isPaused = false;
+            Debug.Log("Testing Resume");
+        }
+*/
+    }
 
     public void Resume()
     {
+
         GameManager.isPaused = false;
         Debug.Log("Resume");
+
     }
 
     public void Restart()
