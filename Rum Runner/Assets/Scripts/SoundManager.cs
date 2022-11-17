d56f2da6e2edf05a351c2f911e24b9bae177f0d8
hba7654 <54544232+hba7654@users.noreply.github.com>
2022-11-16 21:59:54 -0500

    Changed sounds so that they can overlap

--- Rum Runner/Assets/Scripts/SoundManager.cs
+++ Rum Runner/Assets/Scripts/SoundManager.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour
 {
     [Header("Sounds")]
-    [SerializeField] AudioSource[] jumps;
+    private AudioSource audioSource;
+    [SerializeField] AudioClip[] jumps;
     [SerializeField] AudioSource run;
-    [SerializeField] AudioSource[] dies;
+    [SerializeField] AudioClip[] dies;
     //[SerializeField] AudioSource[] darts;
-    [SerializeField] AudioSource bottle;
-    [SerializeField] AudioSource pickup;
-    [SerializeField] AudioSource shoot;
+    [SerializeField] AudioClip bottle;
+    [SerializeField] AudioClip pickup;
+    [SerializeField] AudioClip shoot;
     // Start is called before the first frame update
     void Start()
     {
-
+        audioSource = GetComponent<AudioSource>();
     }
 
     public void PlaySound(string sound)
@@ -24,7 +25,7 @@ public class SoundManager : MonoBehaviour
         {
             case "jump":
                 Debug.Log("JUMP");
-                jumps[Random.Range(0, jumps.Length)].Play();
+                audioSource.PlayOneShot(jumps[Random.Range(0, jumps.Length)], 0.24f);
                 break;
             case "run":
                 Debug.Log("RUN");
@@ -32,21 +33,21 @@ public class SoundManager : MonoBehaviour
                 break;
             case "die":
                 Debug.Log("DIE");
-                dies[Random.Range(0, dies.Length)].Play();
+                audioSource.PlayOneShot(dies[Random.Range(0, dies.Length)]);
                 break;
             //case "dart":
             //    break;
             case "bottle":
                 Debug.Log("BOTTLE");
-                bottle.Play();
+                audioSource.PlayOneShot(bottle);
                 break;
             case "pickup":
                 Debug.Log("PICKUP");
-                pickup.Play();
+                audioSource.PlayOneShot(pickup, 0.2f);
                 break;
             case "shoot":
                 Debug.Log("SHOOT");
-                shoot.Play();
+                audioSource.PlayOneShot(shoot, 0.2f);
                 break;
         }
     }
@@ -55,32 +56,112 @@ public class SoundManager : MonoBehaviour
     {
         switch (sound)
         {
-            case "jump":
-                Debug.Log("JUMP");
-                jumps[Random.Range(0, jumps.Length)].Stop();
-                break;
+            //case "jump":
+            //    Debug.Log("JUMP");
+            //    jumps[Random.Range(0, jumps.Length)].Stop();
+            //    break;
             case "run":
                 Debug.Log("RUN");
                 run.Stop();
                 break;
-            case "die":
-                Debug.Log("DIE");
-                dies[Random.Range(0, dies.Length)].Stop();
-                break;
-            //case "dart":
-            //    break;
-            case "bottle":
-                Debug.Log("BOTTLE");
-                bottle.Stop();
-                break;
-            case "pickup":
-                Debug.Log("PICKUP");
-                pickup.Stop();
-                break;
-            case "shoot":
-                Debug.Log("SHOOT");
-                shoot.Stop();
-                break;
+                //case "die":
+                //    Debug.Log("DIE");
+                //    dies[Random.Range(0, dies.Length)].Stop();
+                //    break;
+                ////case "dart":
+                ////    break;
+                //case "bottle":
+                //    Debug.Log("BOTTLE");
+                //    bottle.Stop();
+                //    break;
+                //case "pickup":
+                //    Debug.Log("PICKUP");
+                //    pickup.Stop();
+                //    break;
+                //case "shoot":
+                //    Debug.Log("SHOOT");
+                //    shoot.Stop();
+                //    break;
         }
     }
+
+    //[Header("Sounds")]
+    //[SerializeField] AudioSource[] jumps;
+    //[SerializeField] AudioSource run;
+    //[SerializeField] AudioSource[] dies;
+    ////[SerializeField] AudioSource[] darts;
+    //[SerializeField] AudioSource bottle;
+    //[SerializeField] AudioSource pickup;
+    //[SerializeField] AudioSource shoot;
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+
+    //}
+
+    //public void PlaySound(string sound)
+    //{
+    //    switch (sound)
+    //    {
+    //        case "jump":
+    //            Debug.Log("JUMP");
+    //            jumps[Random.Range(0, jumps.Length)].Play();
+    //            break;
+    //        case "run":
+    //            Debug.Log("RUN");
+    //            run.Play();
+    //            break;
+    //        case "die":
+    //            Debug.Log("DIE");
+    //            dies[Random.Range(0, dies.Length)].Play();
+    //            break;
+    //        //case "dart":
+    //        //    break;
+    //        case "bottle":
+    //            Debug.Log("BOTTLE");
+    //            bottle.Play();
+    //            break;
+    //        case "pickup":
+    //            Debug.Log("PICKUP");
+    //            pickup.Play();
+    //            break;
+    //        case "shoot":
+    //            Debug.Log("SHOOT");
+    //            shoot.Play();
+    //            break;
+    //    }
+    //}
+
+    //public void StopSound(string sound)
+    //{
+    //    switch (sound)
+    //    {
+    //        case "jump":
+    //            Debug.Log("JUMP");
+    //            jumps[Random.Range(0, jumps.Length)].Stop();
+    //            break;
+    //        case "run":
+    //            Debug.Log("RUN");
+    //            run.Stop();
+    //            break;
+    //        case "die":
+    //            Debug.Log("DIE");
+    //            dies[Random.Range(0, dies.Length)].Stop();
+    //            break;
+    //        //case "dart":
+    //        //    break;
+    //        case "bottle":
+    //            Debug.Log("BOTTLE");
+    //            bottle.Stop();
+    //            break;
+    //        case "pickup":
+    //            Debug.Log("PICKUP");
+    //            pickup.Stop();
+    //            break;
+    //        case "shoot":
+    //            Debug.Log("SHOOT");
+    //            shoot.Stop();
+    //            break;
+    //    }
+    //}
 }
