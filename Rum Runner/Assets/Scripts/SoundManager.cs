588624844a7bfe7d8d12321d8102be81879aedcc
hba7654 <54544232+hba7654@users.noreply.github.com>
2022-12-01 00:41:11 -0500

    Added music to all game screens :)

--- Rum Runner/Assets/Scripts/SoundManager.cs
+++ Rum Runner/Assets/Scripts/SoundManager.cs
@@ -13,10 +13,23 @@ public class SoundManager : MonoBehaviour
     [SerializeField] AudioClip bottle;
     [SerializeField] AudioClip pickup;
     [SerializeField] AudioClip shoot;
+    [SerializeField] AudioSource music;
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if(GameManager.isPaused)
+        {
+            music.volume = 0.02f;
+        }
+        else
+        {
+            music.volume = 0.1f;
+        }
     }
 
     public void PlaySound(string sound)
@@ -24,25 +37,25 @@ public class SoundManager : MonoBehaviour
         switch (sound)
         {
             case "jump":
-                audioSource.PlayOneShot(jumps[Random.Range(0, jumps.Length)], 0.24f);
+                audioSource.PlayOneShot(jumps[Random.Range(0, jumps.Length)]);
                 break;
             case "run":
                 run.Play();
                 break;
             case "die":
-                audioSource.PlayOneShot(dies[Random.Range(0, dies.Length)], 0.24f);
+                audioSource.PlayOneShot(dies[Random.Range(0, dies.Length)]);
                 break;
             case "dart":
-                audioSource.PlayOneShot(darts[Random.Range(0, darts.Length)], 0.24f);
+                audioSource.PlayOneShot(darts[Random.Range(0, darts.Length)]);
                 break;
             case "bottle":
                 audioSource.PlayOneShot(bottle);
                 break;
             case "pickup":
-                audioSource.PlayOneShot(pickup, 0.2f);
+                audioSource.PlayOneShot(pickup);
                 break;
             case "shoot":
-                audioSource.PlayOneShot(shoot, 0.2f);
+                audioSource.PlayOneShot(shoot);
                 break;
         }
     }
